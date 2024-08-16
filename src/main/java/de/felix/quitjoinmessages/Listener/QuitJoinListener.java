d363a037282e89088d95998528cd33993a4de12e
fridu <fridulin21@gmail.com>
2024-08-16 16:59:44 +0200

    Config edit

--- src/main/java/de/felix/quitjoinmessages/Listener/QuitJoinListener.java
+++ src/main/java/de/felix/quitjoinmessages/Listener/QuitJoinListener.java
@@ -1,4 +1,24 @@
 package de.felix.quitjoinmessages.Listener;
 
-public class JoinListener {
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class QuitJoinListener extends JavaPlugin implements Listener {
+    @EventHandler
+    public void onLogin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        String message = getConfig().getString("messages.join").replace("{player}",player.getName());
+        event.setJoinMessage(message);
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        String message = getConfig().getString("messages.quit").replace("{player}",player.getName());
+        event.setQuitMessage(message);
+    }
 }
--- src/main/java/de/felix/quitjoinmessages/QuitJoinMessages.java
+++ src/main/java/de/felix/quitjoinmessages/QuitJoinMessages.java
@@ -1,13 +1,17 @@
 package de.felix.quitjoinmessages;
 
+import de.felix.quitjoinmessages.Listener.QuitJoinListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class QuitJoinMessages extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        saveDefaultConfig();
+        pluginManager.registerEvents(new QuitJoinListener(),this);
     }
 
     @Override
